e60bd89c3465c5cb26b4cd85326a5137af5e349c
AntonR8 <129757394+AntonR8@users.noreply.github.com>
2024-10-15 16:16:56 +0300

    Initial Commit

---
+++ TikTokApp/TikTokAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokAppApp.swift
+//  TikTokApp
+//
+//  Created by Антон Разгуляев on 15.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct TikTokAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
